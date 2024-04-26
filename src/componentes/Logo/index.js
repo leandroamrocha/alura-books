b9844f7ca67bd623d4c32c283638b03c9090b698
Leandro Rocha <leandroamrocha@gmail.com>
2024-04-26 19:20:02 -0300

    excluindo css - criando styled components

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,12 +1,20 @@
 import logo from '../../imagens/logo.svg'
-import './estilo.css'
+import styled from 'styled-components'
+
+const Logotipo = styled.div`
+display: flex;
+    font-size: 30px;
+`
+const Imagem = styled.img`
+margin-right: 10px;
+`
 
 function Logo () {
     return(
-        <div className='logo'>
-          <img src={logo} alt='logo' className='img'></img>
+        <Logotipo>
+          <Imagem src={logo} alt='logo' className='img'></Imagem>
           <p><strong>Alura</strong>Books</p>
-        </div>
+        </Logotipo>
     )
 }
 
